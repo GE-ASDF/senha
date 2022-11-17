e433375f7c8c10d35e6bc40eb1960bec2b3cbba1
Anderson Souza <andersonsouza007@live.com>
2022-11-16 21:17:59 -0300

    Alterei a interface, e separei as funções do script

---
+++ assets/js/objetos.js
@@ -0,0 +1,12 @@
+const optionsObject = {
+    operandoBase: 5,
+    plus: 1,
+    finalBase: '333'
+  }
+
+const options = {
+	timeZone: 'America/Sao_Paulo',
+	hour: 'numeric',
+	minute: 'numeric',
+  second:'numeric'
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,48 +3,64 @@ const $ = (selector) => document.querySelector(selector)
 const passwordInput = $("#password")
 const clockContainer = $("#clock")
 const message = $("#message")
-const optionsObject = {
-  operandoBase: 5,
-  plus: 1,
-  finalBase: '333'
-}
+let objectDate = '';
 
-function getMinuteAndSecond(){
-  const objectDate = new Date();
-  const options = {
-	timeZone: 'America/Sao_Paulo',
-	hour: 'numeric',
-	minute: 'numeric',
-  second:'numeric'
-};
-  
+function getMinuteAndSecond(objectDate){
+
   let secondDigitMinute = Number(objectDate.getMinutes().toString()[1])
-const date = new Intl.DateTimeFormat([], options);
-let clock = date.format(objectDate);
-clockContainer.innerHTML = clock;
   let firstDigitSeconds = Number(objectDate.getSeconds().toString().length) > 1 ? Number(objectDate.getSeconds().toString()[0]):0;
  
   return {secondDigitMinute, firstDigitSeconds}
 }
 
 
-function passwordCalc({operandoBase, plus, finalBase}){
-  return getMinuteAndSecond().secondDigitMinute * operandoBase + plus + getMinuteAndSecond().firstDigitSeconds + finalBase;
+function passwordCalc({operandoBase, plus, finalBase}, {secondDigitMinute, firstDigitSeconds}){
+  return secondDigitMinute * operandoBase + plus + firstDigitSeconds + finalBase;
 }
 
+function verifiedPassword(e){
 
-passwordInput.addEventListener("keyup", ()=>{
+  let getSecondAndFirstObject = getMinuteAndSecond(objectDate)
   let passwordValue = Number(passwordInput.value)
- 
-    if(passwordValue){
-      if(passwordValue === Number(passwordCalc(optionsObject))){
-        message.innerHTML = `<p class="correct">As senhas coincidem </p>`;
-      }else{
-        message.innerHTML = `<p class="incorrect">Senha incorreta! Tente novamente. </p>`
-      }
+  let chave = e.key.toLowerCase();
+  
+  if(chave === 'enter'){
+    if(verifyPassword(getSecondAndFirstObject, passwordValue, chave)){
+      message.innerHTML = printMessage('p', 'correct', 'A senha está correta!');
+      passwordInput.value = ''
     }else{
-      message.innerHTML = "Preencha o campo de senha."
+      message.innerHTML = printMessage('p', 'incorrect', 'Senha incorreta! Tente novamente.');
+      passwordInput.value = ''
     }
-})
+  }
+  
+}
 
-setInterval(getMinuteAndSecond,  2000)
+function verifyPassword(getSecondAndFirstObject, passwordValue){
+
+    if(passwordValue){
+      if(passwordValue === Number(passwordCalc(optionsObject, getSecondAndFirstObject))){
+       return true;
+      }else{
+        return false;
+      }
+  }
+
+}
+
+function printMessage(tag, className, message){
+    return `<${tag} class="${className}">${message}</${tag}>`;
+}
+
+function printDate(objectDate, options){
+  const date = new Intl.DateTimeFormat([], options);
+  let clock = date.format(objectDate);
+  clockContainer.innerHTML = clock;
+}
+
+passwordInput.addEventListener("keypress", verifiedPassword)
+
+setInterval(()=>{
+  objectDate = new Date();
+  printDate(objectDate, options)
+},  1000)
